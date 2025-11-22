ad1d384f7c387aded1537b5cb27fd7b30ffebb33
KARINA <u24263261@utp.edu.pe>
2025-11-22 06:26:13 -0500

    Update SerializadorUsuario.java

--- PROYECTO POO/src/gestorAplicacion/Cuenta/SerializadorUsuario.java
+++ PROYECTO POO/src/gestorAplicacion/Cuenta/SerializadorUsuario.java
@@ -2,27 +2,46 @@ package gestorAplicacion.Cuenta;
 
 import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
+// Esta clase la uso solo para guardar y cargar un Usuario en un archivo XML.
+// La idea es tener algo sencillo para demostrar la parte de "clases persistentes"
+// que vimos en el PPT, sin hacer un sistema de archivos muy complejo.
 public class SerializadorUsuario {
 
+    // Este método guarda un objeto Usuario en un archivo XML.
+    // Lo llamo cuando quiero que el usuario quede "guardado" de forma permanente.
     public static void guardar(Usuario usuario) {
         try {
+            // XMLEncoder convierte el objeto en formato XML y lo escribe en un archivo.
             XMLEncoder encoder = new XMLEncoder(new FileOutputStream("usuario.xml"));
+            
+            // Aquí escribo el objeto completo.
             encoder.writeObject(usuario);
+            
+            // Cierro para que se termine de escribir bien el archivo.
             encoder.close();
         } catch (Exception e) {
+            // Si algo sale mal simplemente imprimo el error.
             e.printStackTrace();
         }
     }
 
+    // Este método lee el archivo XML y reconstruye un Usuario.
+    // Lo uso cuando quiero recuperar el usuario guardado anteriormente.
     public static Usuario cargar() {
         try {
+            // XMLDecoder hace lo contrario: toma el archivo XML y lo transforma en un objeto.
             XMLDecoder decoder = new XMLDecoder(new FileInputStream("usuario.xml"));
+            
+            // Leo el objeto y lo guardo en una variable.
             Usuario usuario = (Usuario) decoder.readObject();
+            
             decoder.close();
             return usuario;
         } catch (Exception e) {
+            // Si el archivo no existe o está vacío, simplemente regreso null.
             return null;
         }
     }
